f96c290608e6b8bc6560a2f114b9afdcc626893e
Hikmah96 <hikmatyusuf2018@gmail.com>
2024-08-05 14:21:02 +0000

    my first commit

---
+++ src/main/java/com/jendereytech/devopsenginerinfo/DevopsenginerinfoApplication.java
@@ -0,0 +1,13 @@
+package com.jendereytech.devopsenginerinfo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevopsenginerinfoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevopsenginerinfoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jendereytech/devopsenginerinfo/DevopsenginerinfoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jendereytech.devopsenginerinfo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevopsenginerinfoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
